44c6c0053f34d6dd924aef3709ff3d5bf178a219
Jason2592 <formosa104@yahoo.com.tw>
2022-05-02 17:00:11 +0800

    Update package name and version for building .whl file.

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ def read(fname):
 
 
 setup(
-    name='xapian-haystack',
-    version='3.0.1',
+    name='xapian-haystack-Iuno',
+    version='3.0.1.1',
     description='A Xapian backend for Haystack',
     long_description=read('README.rst'),
     long_description_content_type='text/x-rst',
